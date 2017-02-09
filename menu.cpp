6de5990234a41c07f3893e6b81adb94143be6d5f
mzavari <mz76.mohammad.zavvari@gmail.com>
2017-02-09 21:35:05 +0330

    add background

--- menu.cpp
+++ menu.cpp
@@ -3,12 +3,18 @@
 #include "game.h"
 #include "setting.h"
 #include "certain.h"
+#include "buy.h"
 extern Game * game;
 Setting * setting;
 Menu::Menu(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Menu)
 {
+    QPixmap bkgnd(":/pic/Picture/image.jpg");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Background, bkgnd);
+    this->setPalette(palette);
     ui->setupUi(this);
 }
 
@@ -29,4 +35,11 @@ void Menu::on_pushButton_3_clicked()
     Certain * certain = new Certain();
     certain->setWindowTitle("Warning!");
     certain->show();
+}
+
+void Menu::on_pushButton_2_clicked()
+{
+    Buy * buy = new Buy();
+    buy->setWindowTitle("Trial Time");
+    buy->show();
 }
